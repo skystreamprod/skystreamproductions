2169006c3a8855cb4c5181f674d0f09f07004589
skystreamprod <skystreamprod@gmail.com>
2024-07-12 19:57:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var prevScrollpos = window.pageYOffset;
+        var hidePoint = 500;
+        
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+        
+            if (currentScrollPos > hidePoint) {
+                if (prevScrollpos > currentScrollPos) {
+                    document.getElementById("header").style.top = "0";
+                    document.getElementById("top").style.top = "40px";
+                } else {
+                    document.getElementById("header").style.top = "-40px";
+                    document.getElementById("top").style.top = "-100px";
+                }
+            }
+        
+            prevScrollpos = currentScrollPos;
+        
+            var video = document.querySelector('.main_video');
+            var parallaxSpeed = 1;
+            var offset = window.pageYOffset * parallaxSpeed;
+            video.style.transform = 'translate3d(-50%, ' + offset + 'px, 0)';
+        }
