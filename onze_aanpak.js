e7eed866a531b03250e981360d4d20695751ac1d
skystreamprod <skystreamprod@gmail.com>
2024-08-05 18:31:46 +0200

    Add files via upload

---
+++ onze_aanpak.js
@@ -0,0 +1,18 @@
+var prevScrollpos = window.pageYOffset;
+        var hidePoint = 10;
+        
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+        
+            if (currentScrollPos > hidePoint) {
+                if (prevScrollpos > currentScrollPos) {
+                    document.getElementById("header").style.top = "0";
+                    document.getElementById("top").style.top = "40px";
+                } else {
+                    document.getElementById("header").style.top = "-40px";
+                    document.getElementById("top").style.top = "-100px";
+                }
+            }
+        
+            prevScrollpos = currentScrollPos;
+        }
