449619b153e1fea47fc612c9f70fa46795f05758
skystreamprod <skystreamprod@gmail.com>
2025-01-30 15:56:54 +0100

    index.js fix

---
+++ index.js
@@ -0,0 +1,24 @@
+var prevScrollpos = window.pageYOffset;
+        var hidePoint = 500;
+        var hidePoint = 600;
+        
+        window.onscroll = function() {
+            var currentScrollPos = window.pageYOffset;
+        
+            if (currentScrollPos > hidePoint) {
+                if (prevScrollpos > currentScrollPos) {
+                    document.getElementById("header").style.top = "0";
+                    document.getElementById("top").style.top = "40px";
+                } else {
+                    document.getElementById("header").style.top = "-40px";
+                    document.getElementById("top").style.top = "-100px";
+                }
+            }
+        
+            prevScrollpos = currentScrollPos;
+        
+            var video = document.querySelector('.main_video');
+            var parallaxSpeed = 0.5;
+            var offset = window.pageYOffset * parallaxSpeed;
+            video.style.transform = 'translate3d(-50%, ' + offset + 'px, 0)';
+        }
